97e26d0fb70da6e482f67249ac2c03682ad11512
Steven Mudie <stevenmudie@icloud.com>
2022-07-14 15:29:24 +1000

    updates features

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,39 +1,54 @@
+import { slide as Menu } from "react-burger-menu";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import GithubIcon from "../images/GitHub.png";
 import EmailIcon from "../images/emailicon.png";
+import "../styles/burgerMenu.css";
 
 const Navbar = (props) => {
   return (
     <header>
-      <div className="logo">
-        Steven <span>Mudie</span>
-      </div>
-      <nav>
+      <div className="container">
+        <div className="wrap">
+          <Menu>
+            <a href="/">Home</a>
+            <a href="/projects">Projects</a>
+            <a href="/contact">Contact</a>
+          </Menu>
+          <div className="logo">
+            Steven <span>Mudie</span>
+          </div>
+        </div>
+        <nav className="mainLinks">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="projects">Projects</Link>
+            </li>
+            <li>
+              <Link to="contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <a
+              href="https://github.com/StevenMudie"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <img src={GithubIcon} alt="Github" />
+            </a>
           </li>
           <li>
-            <Link to="projects">Projects</Link>
-          </li>
-          <li>
-            <Link to="contact">Contact</Link>
+            <a href="mailto:stevenmudie@icloud.com">
+              <img src={EmailIcon} alt="Email" />
+            </a>
           </li>
         </ul>
-      </nav>
-      <ul>
-        <li>
-          <a href="https://github.com/StevenMudie">
-            <img src={GithubIcon} alt="Github" />
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <img src={EmailIcon} alt="Email" />
-          </a>
-        </li>
-      </ul>
+      </div>
     </header>
   );
 };
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,38 @@
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        form.current,
+        process.env.REACT_APP_PUBLIC_KEY
+      )
+      .then(
+        (result) => console.log(result.text),
+        (error) => console.log(error.text)
+      );
+    e.target.reset();
+  };
+
   return (
-    <div>
-      <p>Contact page</p>
-    </div>
+    <form ref={form} onSubmit={sendEmail}>
+      <label>First Name</label>
+      <input type="text" name="firstName" />
+      <label>Last Name</label>
+      <input type="text" name="lastName" />
+      <label>Email</label>
+      <input type="email" name="userEmail" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="send" />
+    </form>
   );
 };
 
